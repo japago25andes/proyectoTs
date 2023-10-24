54ffb8c006cf3f2a474bb14fb87e7c0c2083f7f0
John Alexander Pardo Gonzalez <j.pardog@uniandes.edu.co>
2023-10-24 14:51:39 -0500

    Inicio segundo ciclo

--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
 import { Aprendiz, NivelEducativo } from "./aprendiz.js";
 
-export const ap = new Aprendiz("John Alexander", "Pardo Gonzalez", "avatar.png", 33, NivelEducativo.POSGRADO);
-//commit
-console.log(ap);
+export const ap = new Aprendiz("John Alexander", "Pardo Gonzalez", "avatar.jpg", 33, NivelEducativo.POSGRADO);
+
+let aprendizTable : HTMLElement = document.getElementById("aprendiz")!;
+
+mostrarDatosPrendiz(ap);
+
+function mostrarDatosPrendiz(aprendiz: Aprendiz): void {
+
+    let tbodyAprendiz = document.createElement("tbody");
+
+    tbodyAprendiz.innerHTML = `<tr><td colspan=2><img src="./${aprendiz.avatar}" height="100"></td></tr>
+    <tr><td>Nombres:</td><td>${aprendiz.nombres}</td></tr>
+    <tr><td>Apellidos:</td><td>${aprendiz.apellidos}</td></tr>
+    <tr><td>Nivel Educativo:</td><td>${aprendiz.nivelEducativo}</td></tr>
+    <tr><td>Edad:</td><td>${aprendiz.edad}</td></tr>`
+
+    aprendizTable.appendChild(tbodyAprendiz);
+
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
 import { Aprendiz, NivelEducativo } from "./aprendiz.js";
-export var ap = new Aprendiz("John Alexander", "Pardo Gonzalez", "avatar.png", 33, NivelEducativo.POSGRADO);
-console.log(ap);
+export var ap = new Aprendiz("John Alexander", "Pardo Gonzalez", "avatar.jpg", 33, NivelEducativo.POSGRADO);
+var aprendizTable = document.getElementById("aprendiz");
+mostrarDatosPrendiz(ap);
+function mostrarDatosPrendiz(aprendiz) {
+    var tbodyAprendiz = document.createElement("tbody");
+    tbodyAprendiz.innerHTML = "<tr><td colspan=2><img src=\"./".concat(aprendiz.avatar, "\" height=\"100\"></td></tr>\n    <tr><td>Nombres:</td><td>").concat(aprendiz.nombres, "</td></tr>\n    <tr><td>Apellidos:</td><td>").concat(aprendiz.apellidos, "</td></tr>\n    <tr><td>Nivel Educativo:</td><td>").concat(aprendiz.nivelEducativo, "</td></tr>\n    <tr><td>Edad:</td><td>").concat(aprendiz.edad, "</td></tr>");
+    aprendizTable.appendChild(tbodyAprendiz);
+}
